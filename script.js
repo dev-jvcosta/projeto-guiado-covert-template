60569b8ac92539a2cc2c549c728be8983d1f3d0c
xjohne-work <dev.jvcosta@gmail.com>
2025-03-07 19:11:04 -0300

    refactor: Getting the value of the selected currency

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+// Obtendo os elementos do Formulario.
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+const form = document.querySelector("form");
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
-  const hasCharacters = /\D+/g;
+  const hasCharactersRegex = /\D+/g;
 
   // Removendo os caracteres que não são números e substituindo por "".
-  amount.value = amount.value.replace(hasCharacters, '');
+  amount.value = amount.value.replace(hasCharactersRegex, '');
 })
+
+// Captando o evento do submit (enviar) do formulário.
+form.onsubmit = (event) => {
+  event.preventDefault();
+  console.log(currency.value); // retorna o valor do select currency.
+}
